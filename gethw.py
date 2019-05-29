1eec590836c9882577a85a96eeee9491888f0fc6
orenshani7 <orenshani7@gmail.com>
2019-05-29 10:29:39 +0300

    Update gethw.py

--- gethw.py
+++ gethw.py
@@ -1,3 +1,9 @@
+# This script reads the howerly weather page for Tel Aviv, from waether.com and transform the 
+# weather data table intpo a json (and prints the json).
+# The script uses BeautifulSoup to extract the weather table columns and then uses the zip() function to 
+# transpose the data so that it can iterate over the rows for each hour and build a dictionary that is 
+# finally encoded in json and printed
+
 import json
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
@@ -20,8 +26,8 @@ jdict = {}
 for zdat in zip(times, *fields):
 	jdict[zdat[0].text] = {
 		'DESC' : zdat[1].text,
-		'TEMP' : zdat[2].text[0:-1],
-		'FEEL' : zdat[3].text[0:-1],
+		'TEMP' : zdat[2].text[0:-1], #Get rid of the "degrees" character
+		'FEEL' : zdat[3].text[0:-1], #Same here
 		'PRECIP' : zdat[4].text,
 		'HUMIDITY': zdat[5].text,
 		'WIND' : zdat[6].text  
